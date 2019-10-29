0abe96ee0424a4af131678fc52d21e3cfb055fb2
github username <github email address>
2019-10-29 13:11:46 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  # def initalize(this_dog_name="Lassie")
+  #   @this_dog_name = this_dog_name
+  # end
+
+  #setter
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  #getter
+  def name
+    @this_dogs_name
+  end
+end
